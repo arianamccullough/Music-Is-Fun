07db85aeaa03e68d7ae03f8d63db7cc118e77d84
Ari Ajir <mitra.ariana@gmail.com>
2018-11-12 10:05:11 -0700

    commit

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -7,9 +7,27 @@ const itunesService = new ItunesService()
 function drawSongs(results) {
   console.log(results)
   //YOUR CODING STARTS HERE
+  let template = ""
+  for (let i = 0; i < 50; i++) {
+    let song = results[i];
+    template += `
+        <div class="card py-3 my-3 px-2">
+          <img src="${song.albumArt}" class="card-img-top" id="art">
+          <div class="card-body">
+          <audio controls src="${song.preview}"></audio>
+          <div class="card-title">
+          <h4>${song.title}</h4>
+          <h5> ${song.artist}</h5>
+          <h5>${song.collection}</h5>
+          <h5>${song.price}</h5>
+        </div>
+                </div>
 
+                        </div>
 
-
+      `
+  }
+  document.getElementById('songs').innerHTML = template
 }
 
 
--- app/components/Itunes/itunes-service.js
+++ app/components/Itunes/itunes-service.js
@@ -6,8 +6,8 @@ class ItunesService {
   getMusicByArtist(artist) {
     var url = 'https://itunes.apple.com/search?callback=?&term=' + artist;
     // @ts-ignore
-    return $.getJSON(url)
-      .then(res => res.results.map(s => new Song(s)))
+    return $.getJSON(url) //go to that url and get that 
+      .then(res => res.results.map(s => new Song(s))) //data you got back, iterate on this array with a new song, given that song data
       .catch(err => console.log(err))
   }
 }
